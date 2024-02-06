6cfad1809db726a4f061b989a3e78fdb8e7be0b0
benjamin gonzalez <jgonzalez.benjamin@gmail.com>
2024-02-06 17:54:30 -0300

    agregado de state en selects

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import useSelectCoins from '../hooks/useSelectCoins';
 import { coins } from '../data/coins';
+import { useEffect, useState } from 'react';
 
 const InputSubmit = styled.input`
 	background-color: #9499FF ;
@@ -13,6 +14,7 @@ const InputSubmit = styled.input`
 	text-transform: uppercase;
 	border-radius: 5px;
 	transition: background-color ease-in .45s;
+	margin-top: .5rem;
 	&:hover {
 		background-color: #7A7DFE;
 		cursor: pointer;
@@ -20,14 +22,54 @@ const InputSubmit = styled.input`
 `;
 
 const Form = () => {
-	
-	const [SelectCoins] = useSelectCoins('Elegir moneda', coins);
+	const [cryptos, setCryptos] = useState([]);
+	const [error, setError] = useState(false);
 
-    return (
-		<form>
-			<SelectCoins />
-            <InputSubmit type="submit" value={"Cotizar"} />
-        </form>
+	const [stateCoins, SelectCoins] = useSelectCoins('Elegir moneda', coins);
+	const [stateCryptos, SelectCryptos] = useSelectCoins('Elegir crypto', cryptos);
+
+	useEffect(
+        () => {
+            const responce = async () => {
+                const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`// `https://min-api.cryptocompare.com/data/v4/all/exchanges`;
+                const get_api = await fetch(url);
+				const response = await get_api.json();
+				
+				const cryptos = response.Data.map(data => {
+					const obj = {
+						id: data.CoinInfo.Name, text: data.CoinInfo.FullName
+					}
+					return obj;
+				});
+				setCryptos(cryptos);
+            }
+            responce();
+        }, []
+	);
+	
+	const handleSubmit = ev => {
+		ev.preventDefault();
+		if (!stateCoins || !stateCryptos) {
+			setError(true);
+			return;
+		}
+
+		console.log("passed")
+	}
+
+	return (
+		<>
+			{
+				error && <p> Los campos son obligatorios</p>
+			}
+			<form onSubmit={handleSubmit}>
+				<SelectCoins />
+				
+				<SelectCryptos />
+
+				<InputSubmit type="submit" value={"Cotizar"} />
+			</form>
+		</>
     );
 };
 
--- src/hooks/useSelectCoins.jsx
+++ src/hooks/useSelectCoins.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
+import { useState } from "react";
 import styled from '@emotion/styled';
 
 const Label = styled.label`
@@ -7,21 +8,24 @@ const Label = styled.label`
     font-family: 'Lato', sans-serif;
     font-size: 20px;
     font-weight: 500;
-    margin: 15px 0;
+    margin: .25rem 0;
 `;
 const Select = styled.select`
     width: 100%;
     padding: .5rem .7rem;
     border: none;
     border-radius: 5px;
-    margin-bottom: .5rem;
+    margin-bottom: .2rem;
+    margin-top: .2rem;
 `;
 
 const useSelectCoins = (label, options) => {
+    const [state, setState] = useState('');
+
     const SelectCoins = () => (
         <>
             <Label htmlFor="select-coins">{label}</Label>
-            <Select id="select-coins">
+            <Select id="select-coins" value={state} onChange={ev => setState(ev.target.value)}>
                 <option value="">--Seleccionar--</option>
                 {
                     options && options.map(o => (<option key={o.id} value={o.id}>{o.text}</option>))
@@ -29,7 +33,7 @@ const useSelectCoins = (label, options) => {
             </Select>
         </>
     );
-    return [SelectCoins]
+    return [state, SelectCoins]
 }
 
 export default useSelectCoins;
