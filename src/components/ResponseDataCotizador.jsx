620f5a0cb7fab7a00a2b72738370c6498230b99e
benjamin gonzalez <jgonzalez.benjamin@gmail.com>
2024-02-06 19:12:58 -0300

    se argego estilo al listado resultado

--- src/components/ResponseDataCotizador.jsx
+++ src/components/ResponseDataCotizador.jsx
@@ -1,22 +1,63 @@
 /* eslint-disable react/prop-types */
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+    font-family: 'Lato', sans-serif;
+    font-size: 1rem;
+    font-weight: 500;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+const Txt = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Price = styled.p`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #28b151;
+`;
+
+const Open = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #a4dd20;`;
+
+const Img = styled.img`
+    width: 150px;
+    height: 150px;
+    margin: 0 auto;
+`;
+
+
 const ResponseDataCotizador = ({ viewCoins }) => {
-    const { PRICE, HIGHDAY, LOWDAY, IMAGEURL, CHANGEPCT24HOUR, LASTUPDATE } = viewCoins
+    const { PRICE, HIGHDAY, LOWDAY, IMAGEURL, CHANGEPCT24HOUR, LASTUPDATE, OPEN24HOUR } = viewCoins;
+
     return (
-        <div>
+        <Contenedor>
             {
                 viewCoins && (
-                    <>
-                        <p>Precio: <span>{PRICE}</span></p>
-                        <p>Mas alto hoy: <span>{HIGHDAY}</span></p>
-                        <p>Mas bajo hoy: <span>{LOWDAY}</span></p>
-                        <p>Variacion ult. 24hs: <span>{CHANGEPCT24HOUR}</span></p>
-                        <p>Ultima actualizacion:<span>{LASTUPDATE}</span></p>
-                        
-                        <img width={150} height={150} src={"https://cryptocompare.com"+IMAGEURL} alt="image crypto symbol" />
+                    <>  
+                        <div className="width-50">
+                            <Price>Precio: <span>{PRICE}</span></Price>
+                            <Txt>Mas alto hoy: <span>{HIGHDAY}</span></Txt>
+                            <Txt>Mas bajo hoy: <span>{LOWDAY}</span></Txt>
+                            <Txt>Variacion ult. 24hs: <span>{CHANGEPCT24HOUR}</span></Txt>
+                            <Txt>Ultima actualizacion:<span>{LASTUPDATE}</span></Txt>
+                            <Open>Comenzo en : <span>{OPEN24HOUR}</span></Open>
+                        </div>
+
+                        <Img className="width-50" src={"https://cryptocompare.com"+IMAGEURL} alt="image crypto symbol" />
                     </>
                 )
             }
-        </div>
+        </Contenedor>
     );
 };
 
